eead5fc33766e2e440a7c0320f19f22d287a8bc3
Miskler <69055489+Miskler@users.noreply.github.com>
2026-01-29 23:58:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from ow_config import MAIN_URL
+from typing import Optional, Union, Literal
+from datetime import datetime
 
 from games.api_game import router as game_router
 from mods.api_mod import router as mod_router
@@ -16,6 +18,37 @@ from social.api_black_list import router as black_list_router
 from social.api_forum import router as forum_router
 from social.api_forum_comment import router as forum_comment_router
 
+class CustomResponse(Response):
+    def set_cookie(
+        self,
+        key: str,
+        value: str = "",
+        max_age: Optional[int] = None,
+        expires: Union[datetime, int, None] = None,
+        path: str = "/",
+        domain: Optional[str] = None,
+        secure: bool = False,
+        httponly: bool = False,
+        samesite: Literal["lax", "strict", "none"] = "lax",
+        partitioned: bool = False,
+    ) -> None:
+        # Apply defaults if not specified
+        if domain is None:
+            domain = '.openworkshop.miskler.ru'
+        if samesite is None:
+            samesite = 'lax'
+        super().set_cookie(
+            key=key,
+            value=value,
+            max_age=max_age,
+            expires=expires,
+            path=path,
+            domain=domain,
+            secure=secure,
+            httponly=httponly,
+            samesite=samesite,
+            partitioned=partitioned,
+        )
 
 app = FastAPI(
     title="OpenWorkshop.Manager",
@@ -35,14 +68,8 @@ app = FastAPI(
     Оркестратор имеет зависимые микросервисы: MySQL *(заблокирован для использования вне оркестратора)*, Storage *(файловый сервер к которому можно обращаться напрямую)*.
     """,
     redoc_url=MAIN_URL+"/",
-    docs_url=MAIN_URL+"/docs"
-)
-
-app.config.update(
-    SESSION_COOKIE_SAMESITE='Lax',
-    SESSION_COOKIE_DOMAIN='.openworkshop.miskler.ru',
-    REMEMBER_COOKIE_SAMESITE='Lax',
-    REMEMBER_COOKIE_DOMAIN='.openworkshop.miskler.ru'
+    docs_url=MAIN_URL+"/docs",
+    default_response_class=CustomResponse  # Используем кастомный класс
 )
 
 app.add_middleware(
